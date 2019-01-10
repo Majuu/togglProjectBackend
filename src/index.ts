1fa66da70bd32e72e0720ee5e6edd6e2863d7823
Maju <artur.maj94@gmail.com>
2019-01-10 21:55:15 +0100

    tourtrial done up to body-parser

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,40 @@
 import * as express from 'express';
+import * as bodyParser from 'body-parser';
 
-function loggerMiddleware(request: express.Request, response: express.Response, next) {
-  console.log(`${request.method} ${request.path}`);
-  next();
-}
- 
-const app = express();
- 
-app.use(loggerMiddleware);
- 
-app.get('/', (request, response) => {
-  response.send('Hello world!');
-});
- 
-app.listen(5000);
+
+
+
+
+
+
+
+// BODY-PARSER via Postman
+// const app = express();
+//
+// app.use(bodyParser.json());
+//
+// app.post('/', (request, response) => {
+//   response.send(request.body);
+// });
+//
+// app.listen(5000);
+
+// WORKING FIRST HELLO WORLD
+//
+// function loggerMiddleware(request: express.Request, response: express.Response, next) {
+//   console.log(`${request.method} ${request.path}`);
+//   next();
+// }
+// const app = express();
+//
+// app.use(loggerMiddleware);
+// app.use(bodyParser.json());
+//
+// app.get('/', (request, response) => {
+//   response.send('Hello world!');
+// });
+//
+// app.post('/', (request, response) => {
+//   response.send(request.body);
+// });
+// app.listen(5000);
